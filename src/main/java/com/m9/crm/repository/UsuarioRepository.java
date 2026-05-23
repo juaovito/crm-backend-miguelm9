adf301b426e7beb200e0f617104d9260e74543ca
juaovito <jv18473838@gmail.com>
2026-05-23 19:52:33 -0300

    Add files via upload

---
+++ src/main/java/com/m9/crm/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.m9.crm.repository;
+
+import com.m9.crm.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+}
