864fb3f99a036fff94a57d9fc9b8da94bffc75e8
juaovito <jv18473838@gmail.com>
2026-05-25 20:15:39 -0300

    Update ClienteRepository.java

--- src/main/java/com/m9/crm/repository/ClienteRepository.java
+++ src/main/java/com/m9/crm/repository/ClienteRepository.java
@@ -1,1 +1,10 @@
+// src/main/java/com/m9/crm/repository/ClienteRepository.java
+package com.m9.crm.repository;
 
+import com.m9.crm.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    List<Cliente> findByConsultor(String consultor);
+}
