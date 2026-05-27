b782237f0e6e80a004a52f27c8dcba3b0d6043bd
juaovito <jv18473838@gmail.com>
2026-05-26 21:09:29 -0300

    Create RecursoNaoEncontradoException.java

---
+++ src/main/java/com/m9/crm/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.m9.crm.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
