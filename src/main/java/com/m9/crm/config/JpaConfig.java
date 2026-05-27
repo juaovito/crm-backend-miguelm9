c2ad5eb84614e05c445cf4cb75f132e07100947e
juaovito <jv18473838@gmail.com>
2026-05-26 21:05:36 -0300

    Create JpaConfig.java

---
+++ src/main/java/com/m9/crm/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.m9.crm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
