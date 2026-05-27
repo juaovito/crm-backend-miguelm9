d9565cddc7b62a97aa3b8937f7e3a452184dc6db
juaovito <jv18473838@gmail.com>
2026-05-26 21:11:36 -0300

    Update UsuarioRepository.java

--- src/main/java/com/m9/crm/repository/UsuarioRepository.java
+++ src/main/java/com/m9/crm/repository/UsuarioRepository.java
@@ -2,8 +2,12 @@ package com.m9.crm.repository;
 
 import com.m9.crm.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByLogin(String login);
+    boolean existsByLogin(String login);
+    List<Usuario> findByAtivoTrue();
 }
