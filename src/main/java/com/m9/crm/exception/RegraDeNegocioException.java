ceb3bb54d17faa14088f4bacddf9c78c1b411119
juaovito <jv18473838@gmail.com>
2026-05-26 21:09:54 -0300

    Create RegraDeNegocioException.java

---
+++ src/main/java/com/m9/crm/exception/RegraDeNegocioException.java
@@ -0,0 +1,7 @@
+package com.m9.crm.exception;
+
+public class RegraDeNegocioException extends RuntimeException {
+    public RegraDeNegocioException(String mensagem) {
+        super(mensagem);
+    }
+}
